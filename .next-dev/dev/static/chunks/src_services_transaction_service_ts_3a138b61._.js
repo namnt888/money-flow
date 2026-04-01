27f9c313300e58f42c171d90549da8dd9741ab72
Rei Apple <6s8d77qjgp@privaterelay.appleid.com>
2026-04-01 18:18:32 +0700

    chore: exclude build artifacts and finalize service distribute fixes

--- .next-dev/dev/static/chunks/src_services_transaction_service_ts_3a138b61._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_services_8d8ea52f._.js"
-],
-    source: "dynamic"
-});
